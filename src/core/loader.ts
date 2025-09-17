3aed7ad3e65b04de5c0b0806bee946463fac648a
Espryra <espryra@gmail.com>
2025-09-17 05:58:33 +0000

    Added auto updating config.

--- src/core/loader.ts
+++ src/core/loader.ts
@@ -1,5 +1,7 @@
 import Discord from "../modules/discord/main";
 import Server from "../modules/server";
+import Cache from "../utils/cache";
 
+await Cache.Init();
 await Discord.Init();
 await Server.Init();
--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -4,4 +4,16 @@ import FileManager from "./fileManager";
 export default class Cache {
   public static Config: Config = FileManager.Read<Config>(Sources.CONFIG);
   public static Cooldowns: Record<string, Date> = {};
+
+  public static async Init(): Promise<void> {
+    this.AutoUpdate();
+  }
+
+  private static AutoUpdate(): void {
+    setInterval(() => {
+      try {
+        this.Config = FileManager.Read<Config>(Sources.CONFIG);
+      } catch {}
+    }, 1000);
+  }
 }
